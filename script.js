b130d7cdfd522c55db0e2b4ab1f20b2d20550d6e
szpytma <szpytmapawel@hotmail.com>
2022-11-11 15:52:18 +0000

    update on dates message (today/yesterday/ x days ago/ DATE

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const account1 = {
     '2022-09-27T17:01:17.194Z',
     '2022-09-08T14:11:59.604Z',
     '2022-10-01T10:17:24.185Z',
-    '2022-10-28T09:15:04.904Z',
+    '2022-11-08T09:15:04.904Z',
     '2022-11-10T07:42:02.383Z',
     '2022-11-11T21:31:17.178Z',
   ],
@@ -105,6 +105,25 @@ const inputClosePin = document.querySelector('.form__input--pin');
 
 /////////////////////////////////////////////////
 // Functions
+
+const formatMovementDate = function (date) {
+  const calcDaysPassed = (date1, date2) =>
+    Math.round(Math.abs((date2 - date1) / (1000 * 60 * 60 * 24)));
+
+  const daysPassed = calcDaysPassed(new Date(), date);
+  console.log(daysPassed);
+
+  if (daysPassed === 0) return 'Today';
+  if (daysPassed === 1) return 'Yesterday';
+  if (daysPassed <= 7) return `${daysPassed} days ago`;
+  else {
+    const day = `${date.getDate()}`.padStart(2, '0');
+    const month = `${date.getMonth() + 1}`.padStart(2, '0');
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  }
+};
+
 const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = '';
   //TODO: fix sorting method
@@ -114,12 +133,8 @@ const displayMovements = function (acc, sort = false) {
 
   movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
-
     const date = new Date(acc.movementsDates[i]);
-    const day = `${date.getDate()}`.padStart(2, '0');
-    const month = `${date.getMonth() + 1}`.padStart(2, '0');
-    const year = date.getFullYear();
-    const displayDate = `${day}/${month}/${year}`;
+    const displayDate = formatMovementDate(date);
     const html = `
         <div class="movements__row">
           <div class="movements__type movements__type--${type}">${
