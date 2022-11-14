342a3e22941187af87fe6e73d8c17e477644c19a
szpytma <szpytmapawel@hotmail.com>
2022-11-14 13:28:03 +0000

    Intl.DateTimeFormat implemented

--- script.js
+++ script.js
@@ -9,17 +9,17 @@ const account1 = {
   interestRate: 1.2, // %
   pin: 1111,
   movementsDates: [
-    '2022-08-12T10:51:36.790Z',
     '2022-08-11T23:36:17.929Z',
-    '2022-09-27T17:01:17.194Z',
+    '2022-08-12T10:51:36.790Z',
     '2022-09-08T14:11:59.604Z',
+    '2022-09-27T17:01:17.194Z',
     '2022-10-01T10:17:24.185Z',
     '2022-11-08T09:15:04.904Z',
     '2022-11-10T07:42:02.383Z',
     '2022-11-11T21:31:17.178Z',
   ],
-  currency: 'EUR',
-  locale: 'pt-PT', // de-DE
+  currency: 'GBP',
+  locale: 'en-UK', // de-DE
 };
 
 const account2 = {
@@ -37,8 +37,8 @@ const account2 = {
     '2022-11-25T06:04:23.907Z',
     '2022-11-30T09:48:16.867Z',
   ],
-  currency: 'USD',
-  locale: 'en-US',
+  currency: 'PLN',
+  locale: 'pl-PL',
 };
 
 const account3 = {
@@ -52,9 +52,9 @@ const account3 = {
     '2021-04-10T14:43:26.374Z',
     '2021-06-25T18:49:59.371Z',
     '2021-07-26T12:01:20.894Z',
+    '2021-12-25T06:04:23.907Z',
     '2022-01-30T09:48:16.867Z',
     '2022-02-01T13:15:33.035Z',
-    '2021-12-25T06:04:23.907Z',
   ],
   currency: 'USD',
   locale: 'en-US',
@@ -66,9 +66,10 @@ const account4 = {
   interestRate: 1,
   pin: 4444,
   movementsDates: [
+    '2022-10-30T09:48:16.867Z',
+    '2022-10-25T06:03:23.907Z',
     '2022-10-25T06:04:23.907Z',
     '2022-10-25T14:18:46.235Z',
-    '2022-10-30T09:48:16.867Z',
     '2022-11-11T13:15:33.035Z',
   ],
   currency: 'USD',
@@ -107,21 +108,17 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // Functions
 
-const formatMovementDate = function (date) {
+const formatMovementDate = function (date, locale) {
   const calcDaysPassed = (date1, date2) =>
     Math.round(Math.abs((date2 - date1) / (1000 * 60 * 60 * 24)));
 
   const daysPassed = calcDaysPassed(new Date(), date);
-  console.log(daysPassed);
 
   if (daysPassed === 0) return 'Today';
   if (daysPassed === 1) return 'Yesterday';
   if (daysPassed <= 7) return `${daysPassed} days ago`;
   else {
-    const day = `${date.getDate()}`.padStart(2, '0');
-    const month = `${date.getMonth() + 1}`.padStart(2, '0');
-    const year = date.getFullYear();
-    return `${day}/${month}/${year}`;
+    return new Intl.DateTimeFormat(locale).format(date);
   }
 };
 
@@ -135,7 +132,7 @@ const displayMovements = function (acc, sort = false) {
   movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
     const date = new Date(acc.movementsDates[i]);
-    const displayDate = formatMovementDate(date);
+    const displayDate = formatMovementDate(date, acc.locale);
     const html = `
         <div class="movements__row">
           <div class="movements__type movements__type--${type}">${
@@ -212,12 +209,19 @@ btnLogin.addEventListener('click', function (e) {
     containerApp.style.opacity = 100;
 
     const now = new Date();
-    const day = `${now.getDate()}`.padStart(2, '0');
-    const month = `${now.getMonth() + 1}`.padStart(2, '0');
-    const year = now.getFullYear();
-    const hour = `${now.getHours()}`.padStart(2, '0');
-    const minutes = `${now.getMinutes()}`.padStart(2, '0');
-    labelDate.textContent = `${day}/${month}/${year}, ${hour}:${minutes}`;
+    const options = {
+      hour: 'numeric',
+      minute: 'numeric',
+      day: 'numeric',
+      month: 'numeric',
+      year: 'numeric',
+    };
+
+    const locale = currentAccount.locale;
+
+    labelDate.textContent = new Intl.DateTimeFormat(locale, options).format(
+      now
+    );
 
     // Creat the input fields
     inputLoginUsername.value = inputLoginPin.value = '';
