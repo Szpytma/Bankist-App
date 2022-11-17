316e3b1d1a8923eaca0908219f1f1dcfcae7b6f6
szpytma <szpytmapawel@hotmail.com>
2022-11-17 15:52:43 +0000

    tab components implemented on operations

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const scrollingBehavior = {
   behavior: 'smooth',
 };
 
-///////////////////////////////////////
+///////////////////
 // Modal window
 const openModal = function (event) {
   event.preventDefault();
@@ -49,4 +49,27 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 // Button Scrolling
 btnScrollTo.addEventListener('click', function () {
   section1.scrollIntoView(scrollingBehavior);
+});
+
+///////////////////
+// Tabbed component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+  //Guard clause
+  if (!clicked) return;
+
+  //Remove tab
+  tabs.forEach(tab => tab.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+  //Activate tab
+  clicked.classList.add('operations__tab--active');
+  //Activate content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
 });
